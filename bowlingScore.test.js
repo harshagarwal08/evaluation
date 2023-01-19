7e422f0cfb39d342034e9fe676190524d33c2c27
Harsh <harsh821agarwal@gmail.com>
2023-01-19 10:45:44 +0530

    test: add test cases for calculateTotalScore()

---
+++ bowlingScore.test.js
@@ -0,0 +1,15 @@
+const {calculateTotalScore} = require('./bowlingScore')
+
+test('testcase 1', () => {
+    expect(calculateTotalScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+  });
+
+test('testcase 2', () => {
+    expect(calculateTotalScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
+  });
+
+test('testcase 3', () => {
+    expect(calculateTotalScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
+  });
+
+
